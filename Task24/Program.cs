ac2e8cf9edb90a14508a70291897b3560fdd0e61
Nikita <gorbatenko.nik@bk.ru>
2023-03-26 15:04:20 +0700

    сумма цифр от 1 до N из введенного числа

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // 7
+
+int sum = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
+
+int SumNumbers(int num) // num = 7
+{
+int sum = 0;
+for (int i = 1; i <= num; i++)
+    {
+    sum += i; // sum = sum + i
+    }
+    return sum;
+}
