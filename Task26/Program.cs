8c4e0a0b3634f609664f9725517193de7b4607e9
Nikita <gorbatenko.nik@bk.ru>
2023-03-26 15:09:34 +0700

    Колличество цифр в введенном числе

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digitNumbers = DigitNumbers(number);
+Console.WriteLine($"Количество цифр в числе: {digitNumbers}");
+
+int DigitNumbers(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
