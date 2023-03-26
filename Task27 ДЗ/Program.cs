65f36c5f28d2d4b5f72c1af0be57d043c8dd2938
Nikita <gorbatenko.nik@bk.ru>
2023-03-26 15:37:10 +0700

    вывод суммы цифр в веденном числе

---
+++ Task27 ДЗ/Program.cs
@@ -0,0 +1,24 @@
+﻿// 27. Напшите программу, которая принимает на вход число
+// и выдает сумму цифр в числе.
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;  
+    }
+    return result;
+}
+int number = Prompt("Введите число");
+Console.WriteLine($"Сумма всех цифр в числе {number} = {SumAllDigit(number)}");
