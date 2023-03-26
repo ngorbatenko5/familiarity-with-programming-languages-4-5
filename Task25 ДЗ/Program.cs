db93f60a703329be6384c2567599c29cbc6bdabf
Nikita <gorbatenko.nik@bk.ru>
2023-03-26 15:33:06 +0700

    на вход два числа и возведение первого числа в степень второго

---
+++ Task25 ДЗ/Program.cs
@@ -0,0 +1,37 @@
+﻿// 25. Напишите цикл, который принимает на вход два числа
+// (А и В) и возводит число А в натуральную степень В.
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+
+bool ValidateExponent(int exponent) 
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int powerBase = Prompt("Введите основание");
+int exponent = Prompt("Введите показатель");
+if (ValidateExponent(exponent))
+{
+    Console.WriteLine($"число {powerBase} в степени  {exponent} равно {Power(powerBase, exponent)}");
+}
