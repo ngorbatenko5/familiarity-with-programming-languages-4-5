5e67ce77132ade315857b612455390ae2016207f
Nikita <gorbatenko.nik@bk.ru>
2023-04-01 18:07:05 +0700

    перевод числа из 10-чной в другую систему исчесления

---
+++ Task42/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+int num = Prompt("Введите целое десятичное число");
+int main = Prompt("Введите целое десятичное число основание системы исчисления");
+Console.WriteLine(DecToBin(num, main)); 
+
+int DecToBin(int number, int main)
+{
+    int res = 0;
+    int multiplier = 1;
+    while (number > 0)
+    {
+        res += (number % main) * multiplier;
+        number = number / main;
+        multiplier *= 10;
+    }
+    return res;
+}
+int Prompt(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine()); 
+}
