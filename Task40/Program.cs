0e81a7ef4c28a7fa248cb4f883d5f2f73928debf
Nikita <gorbatenko.nik@bk.ru>
2023-04-01 17:19:37 +0700

    Существует ли треугольник с заданными сторонами

---
+++ Task40/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.WriteLine("Введите длину А");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину B");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину C");
+int C = Convert.ToInt32(Console.ReadLine());
+
+bool isExistTriangle = IsExistTriangle(A, B, C);
+Console.WriteLine(isExistTriangle ? "Существует": "Не существует");
+
+bool IsExistTriangle(int cide1, int cide2, int cide3)
+{
+    return cide1 < cide2 + cide3 && cide2 < cide3 + cide1 && cide3 < cide1 + cide2;
+}
