ff6fd9e5546d464b125856f0792319efb1e33fdb
Nikita <gorbatenko.nik@bk.ru>
2023-04-07 07:45:05 +0700

    не доделал

---
+++ Task47ДЗ/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив размером m × n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+
+int[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns]; // 0, 1
+    Random.NextDouble rnd = new Random.NextDouble();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) // arr = array
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2d = CreateMatrixRndDouble(3, 4, -100, 100);
+PrintMatrix(array2d);
